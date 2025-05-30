b64ea68c9c1a4d4370525c4e5630a3b222f823ed
Anish-Bazad <anishbazad@gmail.com>
2025-05-30 09:57:57 +0530

    added validation for mobile number

--- src/app/agent/buyer-details/buyer-details.component.ts
+++ src/app/agent/buyer-details/buyer-details.component.ts
@@ -8,46 +8,49 @@ import { SaleFinaliseService } from 'src/app/services/sale-finalise.service';
   templateUrl: './buyer-details.component.html',
   styleUrls: ['./buyer-details.component.scss'],
 })
-export class BuyerDetailsComponent  implements OnInit {
+export class BuyerDetailsComponent implements OnInit {
   saleForm!: FormGroup;
   propertyData: any;
 
   constructor(private fb: FormBuilder,
-    private finaliseSaleService : SaleFinaliseService,
-     private router : Router,
-  ) {}
+    private finaliseSaleService: SaleFinaliseService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
     this.saleForm = this.fb.group({
       username: ['', [Validators.required]],
-      contactNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      contactNumber: ['', [Validators.required, Validators.pattern('^[0-9]+$'),Validators.minLength(10),
+        Validators.maxLength(10)]],
       email: ['', [Validators.required, Validators.email]],
       salePrice: ['', [Validators.required]],
       saleDate: ['', [Validators.required]],
       propertyName: [''], // from passed data
-  userId: ['']  
+      userId: [''],
+      propertyId: ['']
     });
-    
+
   }
 
   markSaleComplete() {
     if (this.saleForm.valid) {
-      
+
       this.finaliseSaleService.getPropertyData().subscribe((data) => {
         if (data) {
           this.propertyData = data;
           console.log('Received property data:', this.propertyData);
-    
+
           // Optionally you can patch your form here
           this.saleForm.patchValue({
             propertyName: this.propertyData.propertyName,
-            userId: this.propertyData.userId
+            userId: this.propertyData.userId,
+            propertyId: this.propertyData.id
           });
         }
       });
-      const formData = this.saleForm.value; 
+      const formData = this.saleForm.value;
       console.log('Form Submitted!', formData);
-  
+
       this.finaliseSaleService.finaliseSale(formData).subscribe({
         next: (res) => {
           console.log('Sale finalized successfully:', res);
@@ -58,10 +61,17 @@ export class BuyerDetailsComponent  implements OnInit {
           console.error('Error finalizing sale:', err);
         }
       });
-  
+
     } else {
       console.error('Form is invalid');
-      this.saleForm.markAllAsTouched(); 
+      this.saleForm.markAllAsTouched();
+    }
+  }
+
+  onlyDigits(event: KeyboardEvent): void {
+    const inputChar = event.key;
+    if (!/^\d$/.test(inputChar)) {
+      event.preventDefault();
     }
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { StorageService } from './services/storage.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,43 +6,8 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit{
-  userName!: string;
-  userId: any;
-  token!: string;
-  userRole!: string;
-  constructor(
-    private storageService: StorageService,
-    private router: Router,
-  ) {}
-  async ngOnInit() {
-    const userId = await this.storageService.getPreference('userId');
-    this.userId = userId || '';
-    const userRole = await this.storageService.getPreference('role');
-    this.userRole = userRole || '';
-    const token = await this.storageService.getPreference('jwtToken');
-    this.token = token || '';
 
-    if (this.userName && this.userRole && this.token) {
-      this.redirectBasedOnRole(this.userRole);
-    }
+  constructor() {}
+  ngOnInit(): void {
   }
-
-  redirectBasedOnRole(role:any){
-
-    switch (role) {
-      case 'AGENT':
-        this.router.navigate(['/agent']);
-        break;
-      case 'USER':
-        this.router.navigate(['/user']);
-        break;
-      case 'ADMIN':
-        this.router.navigate(['/admin']);
-        break;
-      default:
-        this.router.navigate(['/login']);
-        break;
-    }
-  }
-
 }
